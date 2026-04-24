abab9898b61d351774db7d28c4279707ad24fe39
aMerricke <m.c.summers404@gmail.com>
2026-04-24 10:13:20 -0400

    Made DQN model.

---
+++ dqn_model/old_stuff/dqn.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class DQN(nn.Module):
+
+    def __init__(self, input_dim, output_dim, hidden_dim=256):
+        super(DQN, self).__init__()
+
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
